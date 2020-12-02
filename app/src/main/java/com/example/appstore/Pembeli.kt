18fbb5e9bc276cd0c7b277b04e02a351d00927b9
Denis-oktav <denisoktav27@gmail.com>
2020-12-02 14:23:45 +0700

    anggota 2

---
+++ app/src/main/java/com/example/appstore/Pembeli.kt
@@ -0,0 +1,14 @@
+package com.UAS.apps
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.w3c.dom.Text
+import java.text.DecimalFormat
+
+@Entity(tableName = "pemebeli")
+data class Pembeli (
+        @PrimaryKey val id_pembeli: Int,
+        @ColumnInfo(name = "nama_pembeli") val nama_pembeli: Text?,
+        @ColumnInfo(name = "alamat") val alamat : Text?
+)
---
+++ app/src/main/java/com/example/appstore/Pembeliwrg.kt
@@ -0,0 +1,23 @@
+package com.UAS.apps
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+interface Pembeliwrg {
+    @Query("SELECT * FROM pemebeli")
+    fun getAll(): List<Jabatan>
+
+    @Query("SELECT * FROM pemebeli WHERE id_pembeli IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<Pembeli>
+
+    @Query("SELECT * FROM pembeli WHERE nama_pembeli LIKE :nama_pembeli AND " +
+            "alamat LIKE :divisi LIMIT 1")
+    fun findByName(nama: String, alamat: String): Pembeli
+
+    @Insert
+    fun insertAll(vararg Pembeli: Pembeli)
+
+    @Delete
+    fun delete(pembeli: Pembeli)
+}
